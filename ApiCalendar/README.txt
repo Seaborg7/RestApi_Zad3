280f5181c4440c5b044f8499f090de899cf9a910
Łukasz Bocian <seaborg7@tlen.pl>
2020-01-14 17:48:00 +0100

    Commit

---
+++ ApiCalendar/README.txt
@@ -0,0 +1,9 @@
+Wywołanie:
+localhost:8080/getcalendar?year=[rok]&month=[miesiąc]
+
+Zapytanie generuje kalendarz w formacie .ics dla podanego miesiąca oraz roku.
+Parametrami domyślnymi są year=2020 oraz month=01.
+
+Przykładowe zapytania:
+localhost:8080/getcalendar?year=2020&month=2
+localhost:8080/getcalendar
---
+++ ApiCalendar/src/main/java/com/example/revert/demo/ApiCalendar.java
@@ -0,0 +1,13 @@
+package com.example.revert.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCalendar {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiCalendar.class, args);
+    }
+
+}
